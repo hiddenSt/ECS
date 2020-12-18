5b0cd5c55da33ce3a944d94d2f33543403f005ce
Viktor Repin <hiddenstmail@gmail.com>
2020-12-18 17:06:40 +0700

    engine is implemented

--- include/engine.hpp
+++ include/engine.hpp
@@ -24,6 +24,7 @@ class Engine {
   EntityId CreateEntity();
   void DestroyEntity(const EntityId& entity_id);
   void MakeTic();
+  void SetUp();
   void ShutDown();
 
  private:
@@ -35,7 +36,20 @@ class Engine {
   SystemsManager& system_manager_;
 };
 
-Engine* Engine::instance_ = nullptr;
+template <typename T, typename... Args>
+T* ecs::Engine::AddComponent(const ecs::EntityId& entity_id, Args&&... args) {
+  components_manager_.AddComponent<T>(entity_id, std::forward<Args>(args)...);
+}
+
+template <typename T>
+void Engine::RemoveComponent(const EntityId& entity_id) {
+  components_manager_.RemoveComponent<T>(entity_id);
+}
+
+template <typename T>
+T Engine::GetComponent(const EntityId& entity_id) {
+  return components_manager_.GetComponent<T>(entity_id)>();
+}
 
 }  // namespace ecs
 #endif  // ECS_INCLUDE_ENGINE_HPP_
--- src/engine.cpp
+++ src/engine.cpp
@@ -2,6 +2,8 @@
 
 #include <stdexcept>
 
+ecs::Engine* ecs::Engine::instance_ = nullptr;
+
 ecs::Engine& ecs::Engine::Instance() {
   if (instance_ == nullptr) {
     throw std::logic_error("Engine is not initialized.");
@@ -9,3 +11,24 @@ ecs::Engine& ecs::Engine::Instance() {
 
   return *instance_;
 }
+
+void ecs::Engine::Initialize() {
+}
+
+ecs::EntityId ecs::Engine::CreateEntity() {
+  return entity_manager_.CreateEntity();
+}
+
+void ecs::Engine::MakeTic() {
+  system_manager_.PreUpdate();
+  system_manager_.Update();
+  system_manager_.PostUpdate();
+}
+
+void ecs::Engine::SetUp() {
+  system_manager_.SetUp();
+}
+
+void ecs::Engine::ShutDown() {
+}
+
